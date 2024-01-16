b876bc2a8e54293a8b084af10963d89e71ef7271
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-01-16 09:31:19 +0200

    get hips data

--- src/blueprints/hips.py
+++ src/blueprints/hips.py
@@ -1,13 +1,22 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, Response
 import json
+from datetime import datetime
 
 from src.models.hips import HipsFactory
 
 hips = Blueprint("hips", __name__)
 
 
-@hips.route("/<string:nuts_level>/<string:experiment>/<string:metric>", methods=["GET"])
+@hips.route("/<nuts_level>/<experiment>/<metric>", methods=["GET"])
 def get_hips(nuts_level, experiment, metric):
     Hips = HipsFactory(nuts_level, experiment, metric)
     hips = Hips.objects().first().to_json()
+    return Response(hips, mimetype="application/json", status=200)
+
+
+@hips.route("/<nuts_level>/<experiment>/<metric>/<date>", methods=["GET"])
+def get_hips_date(nuts_level, experiment, metric, date):
+    date = datetime.strptime(date, "%Y-%m-%d")
+    Hips = HipsFactory(nuts_level, experiment, metric)
+    hips = Hips.objects(date=date).to_json()
     return Response(hips, mimetype="application/json", status=200)
