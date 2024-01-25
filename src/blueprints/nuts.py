405f0a8359555672c918e83d243c2e4af9589db1
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-01-25 13:30:28 +0200

    nuts

--- src/blueprints/nuts.py
+++ src/blueprints/nuts.py
@@ -1,27 +1,14 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, Response
 import json
 
-from src.models.nuts import Nuts
+from src.models.nuts import Nuts30
 
 nuts = Blueprint("nuts", __name__)
 
 
-@nuts.route("", methods=["POST"])
-def create_nuts():
-    nut_doc = Nuts(**json.loads(request.data))
-    nut_doc.save()
-
-    return Response(
-        response=nut_doc.to_json(),
-        status=200,
-        mimetype="application/json",
-    )
-
-
 @nuts.route("/<int:nut_level>", methods=["GET"])
 def get_nuts(nut_level: int):
-    print(nut_level, type(nut_level))
-    nut_doc = Nuts.objects(level=nut_level)
+    nut_doc = Nuts30.objects(properties__LEVL_CODE=nut_level)
 
     return Response(
         response=nut_doc.to_json(),
@@ -33,16 +20,14 @@ def get_nuts(nut_level: int):
 @nuts.route("/<int:nut_level>/<string:nuts_id>", methods=["GET"])
 def get_nuts_id(nut_level: int, nuts_id: str):
     print(nut_level, type(nut_level))
-    nut_doc = Nuts.objects(level=nut_level).first()
-
-    wanted_feature = None
-    for feature in nut_doc.featureCollection.features:
-        if feature.nuts_id == nuts_id:
-            wanted_feature = feature
-            break
+    nut_doc = (
+        Nuts30.objects(properties__LEVL_CODE=nut_level, properties__NUTS_ID=nuts_id)
+        .exclude("id")
+        .first()
+    )
 
     return Response(
-        response=wanted_feature.to_json(),
+        response=nut_doc.to_json(),
         status=200,
         mimetype="application/json",
     )
--- src/models/nuts.py
+++ src/models/nuts.py
@@ -1,13 +1,4 @@
 import mongoengine as me
-from src.models.shared import PolygonFeature, MultiPolygonFeature
-
-
-class FeatureCollection(me.EmbeddedDocument):
-    type = me.StringField(required=True, default="FeatureCollection")
-    features = me.ListField(
-        me.GenericEmbeddedDocumentField(choices=[PolygonFeature, MultiPolygonFeature]),
-        required=True,
-    )
 
 
 class MyPolygonField(me.EmbeddedDocument):
@@ -17,15 +8,9 @@ class MyPolygonField(me.EmbeddedDocument):
 
 class MyMultiPolygonField(me.EmbeddedDocument):
     type = me.StringField(required=True, default="MultiPolygon")
-    coordinates = me.ListField(me.ListField(me.ListField(me.ListField(me.FloatField()))))
-
-
-class Nuts(me.Document):
-    level = me.IntField(required=True, choices=[0, 1, 2, 3])
-    scale = me.IntField(required=True, choices=[30])
-    featureCollection = me.EmbeddedDocumentField(FeatureCollection, required=True)
-
-    meta = {"collection": "nuts", "db_alias": "attica_dt"}
+    coordinates = me.ListField(
+        me.ListField(me.ListField(me.ListField(me.FloatField())))
+    )
 
 
 class Nuts30(me.Document):
@@ -33,8 +18,8 @@ class Nuts30(me.Document):
     geometry = me.GenericEmbeddedDocumentField(
         choices=[MyPolygonField, MyMultiPolygonField]
     )
-    
+
     properties = me.DictField(required=True)
-    nuts_id = me.StringField(required=True, db_field="id")
+    nuts_id = me.StringField(required=True)
 
     meta = {"collection": "nuts30", "db_alias": "attica_dt"}
